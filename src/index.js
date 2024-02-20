b5a154c710466168c45e5d05cb6f33e63a28e5d4
Bibekananda Pattanaik <115365879+bibekp1203@users.noreply.github.com>
2024-02-20 21:53:00 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import { Login } from './components/Login';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "login",
+    element: <Login/>,
+  },
+  {
+    path: "signup",
+    element: <Login/>,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+reportWebVitals();
